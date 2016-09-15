e06e79f7313825970bb5a58920db074e6b132dd3
adanlozano <adanlozano11@gmail.com>
2016-09-15 09:23:49 -0400

    Adding install file

---
+++ RMSHQDynazipErrorClearerPowerShell.ps1
@@ -0,0 +1,18 @@
+﻿function Install-RMSHQDynazipErrorClearer{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Computer
+    )
+
+    #$env:USERPROFILE  
+
+    $RMSHQDynazipErrorClearerRepoURL = https://github.com/Tervis-Tumbler/RMSHQDynazipErrorClearer.git
+
+    choco install git
+    
+    git clone $RMSHQDynazipErrorClearerRepoURL
+
+    #($ENV:PSModulepath -split ";")[0]
+}
