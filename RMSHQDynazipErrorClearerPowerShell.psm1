dcbb4902701ee85158dac794ef372ae8bb334871
adanlozano <adanlozano11@gmail.com>
2016-09-16 15:56:04 -0400

    Added function to create scheduled task that runs ahk file on startup

--- RMSHQDynazipErrorClearerPowerShell.psm1
+++ RMSHQDynazipErrorClearerPowerShell.psm1
@@ -1,13 +1,18 @@
-﻿function Install-RMSHQDynazipErrorClearer{
-    [CmdletBinding()]
-    param (
-    )
-    
+﻿function Install-RMSHQDynazipErrorClearer{ 
     $ModulePath = $env:USERPROFILE + '\Documents\WindowsPowerShell\Modules\RMSHQDynazipErrorClearer'
     $AhkShortcutPath = '.\ClearDynazipError.ahk - Shortcut.lnk' 
     $AhkShortcutDestination = $env:USERPROFILE + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\'
-    
     Set-Location $ModulePath
     Copy-Item -Path $AhkShortcutPath -Destination $AhkShortcutDestination
-
+}
+function New-RMSHQDynazipErrorClearerScheduledTask{
+    $AHKPath = $env:USERPROFILE+"\Documents\WindowsPowerShell\Modules\RMSHQDynazipErrorClearer\ClearDynazipError.ahk"
+    $Action = New-ScheduledTaskAction -Execute $AHKPath
+    $Description = "Starts RMSHQDynazipErrorClearer.ahk on System Startup"
+    $Trigger = New-ScheduledTaskTrigger -AtLogOn
+    $User = $env:COMPUTERNAME+"\administrator"
+    $TaskName = "Start-RMSHQDynazipErrorClearer.ahk"
+    $Settings = New-ScheduledTaskSettingsSet
+    $Settings.ExecutionTimeLimit="PT0S"
+    Register-ScheduledTask -TaskName $TaskName -Action $Action -User $User -Trigger $Trigger -Description $Description -Settings $Settings
 }
