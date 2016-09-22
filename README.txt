f1ce33cb6627f6adf60003b73e87b397076ff0bd
adanlozano <adanlozano11@gmail.com>
2016-09-22 12:39:31 -0400

    Update powershell module and readme

--- README.txt
+++ README.txt
@@ -1,7 +1,30 @@
-Place the "RMSHQDynazipErrorClearer" folder in the following directory:
-%USERPROFILE%\Documents\WindowsPowerShell\RMSHQDynazipErrorClearer
+How to install RMSHQDynazipErrorClearer -
 
-Run the following in PowerShell as an Administrator:
+1. Install Chocolatey from PowerShell
+	A. Open PowerShell as an Administrator
+	B. Run the following:
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+2. Install Git
+	A. Open PowerShell as an Administrator
+	B. Run the following:
+choco install git.install -y -f
+
+3. Clone the repository
+	A. Open PowerShell as an Administrator
+	B. Run the following:
+Set-Location $env:USERPROFILE+"\Documents\WindowsPowerShell\Modules\RMSHQDynazipErrorClearer"; Git Clone https://github.com/Tervis-Tumbler/RMSHQDynazipErrorClearer
+	
+4. Create a scheduled task to initiate the program on system startup
+	A. Run the following in PowerShell as an Administrator:
 New-RMSHQDynazipErrorClearerScheduledTask
 
-This will create a scheduled task that will run the EXE on next startup.
+
+How to use RMSHQDynazipErrorClearer -
+1. Once installed the software should run automatically on system startup.
+
+
+How to update RMSHQDynazipErrorClearer -
+1. Open PowerShell as an Administrator
+2. Run the following:
+Get-RMSHQDynazipErrorClearerUpdate
--- RMSHQDynazipErrorClearerPowerShell.psm1
+++ RMSHQDynazipErrorClearerPowerShell.psm1
@@ -1,11 +1,4 @@
-﻿function Install-RMSHQDynazipErrorClearer{ 
-    $ModulePath = $env:USERPROFILE + '\Documents\WindowsPowerShell\Modules\RMSHQDynazipErrorClearer'
-    $AhkShortcutPath = '.\ClearDynazipError.ahk - Shortcut.lnk' 
-    $AhkShortcutDestination = $env:USERPROFILE + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\'
-    Set-Location $ModulePath
-    Copy-Item -Path $AhkShortcutPath -Destination $AhkShortcutDestination
-}
-function New-RMSHQDynazipErrorClearerScheduledTask{
+﻿function New-RMSHQDynazipErrorClearerScheduledTask{
     $AHKPath = $env:USERPROFILE+"\Documents\WindowsPowerShell\Modules\RMSHQDynazipErrorClearer\ClearDynazipError.exe"
     $Action = New-ScheduledTaskAction -Execute $AHKPath
     $Description = "Starts RMSHQDynazipErrorClearer.exe on System Startup"
@@ -15,4 +8,12 @@ function New-RMSHQDynazipErrorClearerScheduledTask{
     $Settings = New-ScheduledTaskSettingsSet 
     $Settings.ExecutionTimeLimit="PT0S"
     Register-ScheduledTask -TaskName $TaskName -Action $Action -User $User -Trigger $Trigger -Description $Description -Settings $Settings -RunLevel Highest
+}
+function Get-RMSHQDynazipErrorClearerUpdate{
+    $RepositoryLocal = $env:USERPROFILE+"\Documents\WindowsPowerShell\Modules\RMSHQDynazipErrorClearer"
+
+    if(Test-Path $RepositoryLocal){
+        Set-Location $RepositoryLocal
+        Git Pull
+    }
 }
